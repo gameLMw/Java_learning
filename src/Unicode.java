f635f54b20fecce8ced78027ff4c84327bbcf363
LM <wycalxy@outlook.com>
2026-01-03 19:53:37 +0800

    feat(learning): 添加Unicode字符遍历程序
    - 创建了Unicode类用于遍历字符编码
    - 实现了从19967到40959的字符编码打印功能
    - 添加了字符编码与对应字符的对照输出
    - 增加分隔线以便清晰显示每个字符信息

---
+++ src/Unicode.java
@@ -0,0 +1,12 @@
+package learning;
+
+public class Unicode {
+    public static void main(String[] args) {
+        for (int i = 19967; i < 40960; i++) {
+            char j = (char) i;
+            System.out.println(i);
+            System.out.println(j);
+            System.out.println("------------------");
+        }
+    }
+}
