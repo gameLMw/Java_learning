6a8333b0d645e484c9fd31fddc453f34a0ed378e
LM <wycalxy@outlook.com>
2026-01-04 21:33:21 +0800

    feat(learning): 添加缓冲区示例程序
    - 创建了 buffer.java 文件
    - 实现了基本的输入输出功能
    - 添加了整数和字符串的读取示例
    - 集成了 Scanner 类进行输入处理

---
+++ src/learning/buffer.java
@@ -0,0 +1,15 @@
+package learning;
+
+import java.util.Scanner;
+
+public class buffer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        String b = sc.next();
+        System.out.println(b);
+    }
+
+
+}
