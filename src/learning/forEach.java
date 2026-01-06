aa6d42ef0f6d5558630dd407d52eaae19286456b
LM <wycalxy@outlook.com>
2026-01-06 22:48:13 +0800

    feat(learning): 添加二维数组遍历示例
    - 添加了二维数组的初始化和遍历代码
    - 实现了嵌套for each循环遍历二维数组
    - 添加了数组遍历的注释说明
    - 扩展了原有一维数组遍历功能

--- src/learning/forEach.java
+++ src/learning/forEach.java
@@ -2,10 +2,21 @@ package learning;
 
 public class forEach {
     public static void main(String[] args) {
+        /*
+        for each循环,遍历数组
+         */
         int[] a = {1, 2, 3, 4, 5};
         for (int i : a) {
             System.out.println(i);
         }
-        // foreach遍历集合（集合类，Iterable接口）
+        int[][] b = {{1, 2, 3},
+                {4, 5,},
+                {6,7, 8, 9}};
+        for(int[] i:b){
+            for(int j:i){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
